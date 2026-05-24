023b7c5f2d51f853194b0917a1f5b994f2259d31
lance1416 <46139702+lance1416@users.noreply.github.com>
2026-05-24 13:52:39 +0100

    chore: scaffold project structure

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,9 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod archive;
+mod encoding;
+
+fn main() {
+    cheesy_toast_archive_manager_lib::run()
+}
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import './styles/root.css';
+
+createApp(App).mount("#app");
